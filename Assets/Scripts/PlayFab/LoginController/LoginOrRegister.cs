b71a2f5f98830e654ca8be7c485ea15f628f293b
Adem Ertaş <70427838+ademeu@users.noreply.github.com>
2022-04-05 14:21:57 +0300

    221

---
+++ Assets/Scripts/PlayFab/LoginController/LoginOrRegister.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginOrRegister : MonoBehaviour
+{
+    PlayerAccount _playerAccount;
+
+    [SerializeField] InputField _loginUsername, _email, _password, _rPassword;
+    [SerializeField] Button _loginOrRegisterButton;
+    [SerializeField] Text _title,_RegisterOrLoginButtonText, _orRegisterLogin;
+
+    public bool _isRegisterActive { get; set; }
+
+    private void Awake()
+    {
+       _playerAccount = new PlayerAccount();
+      // _loginOrRegisterButton.interactable = false;
+    }
+    public void Register()
+    {
+        _playerAccount.LoginUsername = _loginUsername.text;
+        _playerAccount.Email = _email.text;
+        _playerAccount.Password = _password.text;
+        _playerAccount.RepeatPassword = _rPassword.text;
+        _playerAccount.DisplayName = _loginUsername.text;
+        _playerAccount.Register();
+    }
+    //public void InputCheckControl()
+    //{
+    //    if (_loginUsername.text.Length >= 6 && (_password.text == _rPassword.text) && (_password.text != "") && _password.text.Length >= 6)
+    //    {
+    //        _loginOrRegisterButton.interactable = true;
+    //    }
+    //    else
+    //    {
+    //        _loginOrRegisterButton.interactable = false;
+    //    }
+    //}
+   
+    //public void InputPasswordColorConrrol()
+    //{
+    //    if (_password.text != _rPassword.text || (_password.text.Length < 6))
+    //    {
+    //        _password.image.color = Color.red;
+    //        _rPassword.image.color = Color.red;
+    //    }
+    //    else
+    //    {
+    //        _password.image.color = Color.green;
+    //        _rPassword.image.color = Color.green;
+    //    }
+    //}
+    //public void InputUsernameColorControl()
+    //{
+    //    if (_loginUsername.text.Length > 6)
+    //    {
+    //        _loginUsername.image.color = Color.green;
+    //    }
+      
+    //    else
+    //    {
+    //        _loginUsername.image.color = Color.red;
+    //    }
+    //}
+    //public void InputEmailColorControl()
+    //{
+    //    if (_email.text.IndexOf('@') <= 0)
+    //    {
+    //        _email.image.color = Color.red;
+    //    }
+
+    //    else
+    //    {
+    //        _email.image.color = Color.green;
+    //    }
+    //}
+
+    public void Login()
+    {
+        _playerAccount.LoginUsername = _loginUsername.text;
+        _playerAccount.Password = _password.text;
+        _playerAccount.Login();
+    }
+
+    public void RegisterOrLoginPanelControl()
+    {
+        switch (!_email.gameObject.activeInHierarchy)
+        {
+            case true:
+                RegisterPanel();
+                break;
+            case false:
+                LoginPanel();
+                break;
+        }
+    }
+
+    void RegisterPanel()
+    {
+        _title.text = "Register";
+        _email.gameObject.SetActive(true);
+        _rPassword.gameObject.SetActive(true);
+        _RegisterOrLoginButtonText.text = "Register";
+        _orRegisterLogin.text = "Or Login";
+        _loginOrRegisterButton.onClick.RemoveAllListeners(); // butonun ıcındekılerı sılıyor.
+        _loginOrRegisterButton.onClick.AddListener(Register);
+    }
+    void LoginPanel()
+    {
+        _title.text = "Login";
+        _email.gameObject.SetActive(false);
+        _rPassword.gameObject.SetActive(false);
+        _RegisterOrLoginButtonText.text = "Login";
+        _orRegisterLogin.text = "Or Register";
+        _loginOrRegisterButton.onClick.RemoveAllListeners(); // butonun ıcındekılerı sılıyor.
+        _loginOrRegisterButton.onClick.AddListener(Login);
+    }
+}
---
+++ Assets/Scripts/PlayFab/LoginController/PlayerAccount.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using PlayFab; // get işlemi icin yazdık.
+using PlayFab.ClientModels; // set işlemi icin yazdık.
+using System;
+
+public class PlayerAccount 
+{
+    public string PlayerID { get; set; }
+    public string DisplayName { get; set; }
+    public string LoginUsername { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string RepeatPassword { get; set; }
+
+    #region Register
+    public void Register()
+    {
+        RegisterPlayFabUserRequest _registerRequest = new RegisterPlayFabUserRequest() { Username = LoginUsername, Email = Email, Password = Password, DisplayName = DisplayName };
+
+        PlayFabClientAPI.RegisterPlayFabUser(_registerRequest, RegisterSuccess, RegisterError);
+    }
+
+    private void RegisterError(PlayFabError obj)
+    {
+        Debug.LogError("Kayit Basarisiz");
+    }
+
+    private void RegisterSuccess(RegisterPlayFabUserResult obj)
+    {
+        Debug.Log("Kayit Basarili");
+    }
+    #endregion
+
+    #region Login
+    public void Login()
+    {
+        LoginWithPlayFabRequest _loginRequest = new LoginWithPlayFabRequest() { Username = LoginUsername, Password = Password };
+        PlayFabClientAPI.LoginWithPlayFab(_loginRequest, LoginSucces, LoginError);
+    }
+
+    private void LoginError(PlayFabError obj)
+    {
+        Debug.LogError("Giris Basarisiz");
+    }
+
+    private void LoginSucces(LoginResult obj)
+    {
+        Debug.LogError("Giris Basarili");
+    } 
+    #endregion
+}
